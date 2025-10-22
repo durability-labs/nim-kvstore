2f4a1f155697a787255b93755bd0e225ea5e3f54
Dmitriy Ryajov <dryajov@gmail.com>
2025-10-22 10:34:25 -0600

    make datastore typeaware from the ground up

--- datastore.nim
+++ datastore.nim
@@ -1,8 +1,143 @@
-import ./datastore/datastore
+import std/sequtils
+
+import pkg/chronos
+import pkg/questionable
+import pkg/questionable/results
+
+import ./datastore/key
+import ./datastore/datastore as rawds
 import ./datastore/fsds
 import ./datastore/sql
-import ./datastore/leveldb
 import ./datastore/mountedds
 import ./datastore/tieredds
+import ./datastore/types
+import ./datastore/query
 
-export datastore, fsds, mountedds, tieredds, sql, leveldb
+export datastore, fsds, mountedds, tieredds, sql, types, query, key
+
+## Typed Datastore Support
+##
+## Provides automatic conversion between user-defined types and seq[byte]
+## using encoder/decoder procs.
+##
+## Basic usage:
+## .. code-block:: Nim
+##   import pkg/stew/byteutils
+##   import pkg/questionable/results
+##
+##   type Person = object
+##     age: int
+##     name: string
+##
+##   proc encode(p: Person): seq[byte] =
+##     ($p.age & ":" & p.name).toBytes()
+##
+##   proc decode(T: type Person, bytes: seq[byte]): ?!T =
+##     let values = string.fromBytes(bytes).split(':', maxsplit = 1)
+##     success(Person(age: parseInt(values[0]), name: values[1]))
+##
+##   let p1 = Person(name: "john", age: 21)
+##   let record1 = Record[Person].init(key, p1)
+##   discard await ds.put(record1)
+##   let record2 = (await ds.get[Person](key)).tryGet()
+##   assert record2.val == p1
+
+# Typed operations
+# Note: Converters work for single records, but not for seq[Record[T]] -> seq[RawRecord]
+# So we provide explicit typed wrappers
+
+# Typed Record constructor
+proc init*[T](_: type Record[T], key: Key, val: T, token = 0'u64): Record[T] =
+  Record[T](key: key, val: val, token: token)
+
+# Typed datastore API
+proc get*[T](
+    self: Datastore, keys: seq[Key]
+): Future[?!seq[Record[T]]] {.async: (raises: [CancelledError]).} =
+  ## Get a list of records specified by the keys
+  ##
+
+  let raw = ?(await rawds.get(self, keys))
+  success raw.mapIt( ?toRecord[T](it) )
+
+proc get*[T](
+    self: Datastore, key: Key
+): Future[?!Record[T]] {.async: (raises: [CancelledError]).} =
+  ## Get a single record
+  ##
+
+  toRecord[T]((?(await rawds.get(self, key))))
+
+proc put*[T](
+    self: Datastore, records: seq[Record[T]]
+): Future[?!seq[Key]] {.async: (raw: true, raises: [CancelledError]).} =
+  ## Insert or update a group of records
+  ##
+  ## The sequence will contain records that couldn't be inserted/updated
+  ## due to conflicts.
+  ##
+
+  rawds.put(self, records.mapIt( it.toRaw ))
+
+proc put*[T](
+    self: Datastore, record: Record[T]
+): Future[?!void] {.async: (raw: true, raises: [CancelledError]).} =
+  ## Insert or update a single record
+  ##
+  ## The result contains the record that couldn't be inserted/updated
+  ##
+
+  rawds.put(self, record.toRaw)
+
+proc put*[T](
+    self: Datastore, key: Key, value: T
+): Future[?!void] {.async: (raw: true, raises: [CancelledError]).} =
+  rawds.put(self, Record.init(key, value).toRaw)
+
+proc delete*[T](
+    self: Datastore, records: seq[Record[T]]
+): Future[?!seq[Key]] {.async: (raw: true, raises: [CancelledError]).} =
+  ## Delete a list of records
+  ##
+  ## Returns a sequence of keys that were skipped due to conflicts.
+  ##
+
+  rawds.delete(self, records.mapIt( it.toRaw ))
+
+proc delete*[T](
+    self: Datastore, record: Record[T]
+): Future[?!void] {.async: (raw: true, raises: [CancelledError]).} =
+  ## Delete a single record
+  ##
+  ## Errors if conflict occurs
+  ##
+
+  rawds.delete(self, record.toRaw)
+
+proc contains*(
+    self: Datastore, key: Key
+): Future[bool] {.async: (raises: [CancelledError]).} =
+  return (await rawds.has(self, key)) |? false
+
+proc query*[T](self: Datastore, q: Query): Future[?!QueryIter[T]] {.async: (raises: [CancelledError]).} =
+  let dsIter = ?(await rawds.query(self, q))
+
+  proc next: Future[?!(?Record[T])] {.async: (raises: [CancelledError]).} =
+    let rawOpt = ?(await dsIter.next())
+    if rawOpt.isNone:
+      return success Record[T].none
+
+    let decoded = ?(toRecord[T](rawOpt.get()))
+    success decoded.some
+
+  proc isFinished: bool =
+    dsIter.finished
+
+  proc dispose =
+    dsIter.dispose()
+
+  success QueryIter[T].new(
+    next =  next,
+    finished = isFinished,
+    dispose = dispose
+  )
--- datastore/query.nim
+++ datastore/query.nim
@@ -1,67 +1,72 @@
-
 {.push raises: [].}
 
 import pkg/chronos
 import pkg/questionable
 import pkg/questionable/results
 
-import ./key
 import ./types
+import ./key
 
 type
+  QueryEndedError* = object of DatastoreError
+
   SortOrder* {.pure.} = enum
-    Assending,
+    Assending
     Descending
 
   Query* = object
-    key*: Key         # Key to be queried
-    value*: bool      # Flag to indicate if data should be returned
-    limit*: int       # Max items to return - not available in all backends
-    offset*: int      # Offset from which to start querying - not available in all backends
-    sort*: SortOrder  # Sort order - not available in all backends
+    key*: Key # Key to be queried
+    value*: bool # Flag to indicate if data should be returned
+    limit*: int # Max items to return - not available in all backends
+    offset*: int # Offset from which to start querying - not available in all backends
+    sort*: SortOrder # Sort order - not available in all backends
 
-  QueryResponse* = tuple[key: ?Key, data: seq[byte]]
-  QueryEndedError* = object of DatastoreError
+  IterFinished* = proc(): bool {.gcsafe, closure, raises: [].}
+  IterDispose* = proc() {.gcsafe, closure, raises: [].}
+  GetNext*[T] = proc(): Future[?!(?Record[T])] {.
+    async: (raises: [CancelledError]), gcsafe, closure
+  .}
 
-  GetNext* = proc: Future[?!QueryResponse] {.async: (raises: [CancelledError]).}
-  IterFinished* = proc: bool {.gcsafe, closure, raises:[].}
-  IterDispose* = proc() {.gcsafe, closure, raises:[].}
-  QueryIter* = ref QueryIterObj
-  QueryIterObj = object
-    nextImpl: GetNext
+  QueryIter*[T] = ref object
+    nextImpl: GetNext[T]
     finishedImpl: IterFinished
     disposeImpl: IterDispose
 
-proc finished*(iter: QueryIter): bool =
+  GetNextRaw* = GetNext[seq[byte]]
+  QueryIterRaw* = QueryIter[seq[byte]]
+
+proc finished*[T](iter: QueryIter[T]): bool =
   iter.finishedImpl()
 
-proc next*(iter: QueryIter): Future[?!QueryResponse] {.async: (raises: [CancelledError]).} =
+proc next*[T](
+    iter: QueryIter[T]
+): Future[?!(?Record[T])] {.async: (raises: [CancelledError]).} =
   await iter.nextImpl()
 
-proc dispose*(iter: QueryIter) =
+proc dispose*[T](iter: QueryIter[T]) =
   iter.disposeImpl()
 
-iterator items*(q: QueryIter): Future[?!QueryResponse] =
+iterator items*[T](q: QueryIter[T]): Future[?!(?Record[T])] =
   while not q.finished:
     yield q.next()
 
-proc defaultDispose =
+proc defaultDispose() =
   discard
 
-proc new*(T: type QueryIter, next: GetNext, finished: IterFinished, dispose: IterDispose = defaultDispose): T =
-  QueryIter(nextImpl: next, finishedImpl: finished, disposeImpl: dispose)
+proc new*[T](
+    _: type QueryIter[T],
+    next: GetNext[T],
+    finished: IterFinished,
+    dispose: IterDispose = defaultDispose,
+): QueryIter[T] =
+  QueryIter[T](nextImpl: next, finishedImpl: finished, disposeImpl: dispose)
 
 proc init*(
-  T: type Query,
-  key: Key,
-  value = true,
-  sort = SortOrder.Assending,
-  offset = 0,
-  limit = -1): T =
-
-  T(
-    key: key,
-    value: value,
-    sort: sort,
-    offset: offset,
-    limit: limit)
+    _: type Query,
+    key: Key,
+    value = true,
+    sort = SortOrder.Assending,
+    offset = 0,
+    limit = -1,
+): Query =
+  Query(key: key, value: value, sort: sort, offset: offset, limit: limit)
