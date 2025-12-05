0d06e30180ee7fe18f14c902ca257bf47e82506e
Dmitriy Ryajov <dryajov@gmail.com>
2025-12-05 17:12:08 -0600

    refactor: remove TieredDatastore implementation and associated tests

--- tests/testall.nim
+++ tests/testall.nim
@@ -2,7 +2,6 @@ import ./datastore/testkey
 import ./datastore/testdatastore
 import ./datastore/testfsds
 import ./datastore/testsql
-import ./datastore/testtieredds # this fails silently somewhere
 import ./datastore/testmountedds
 
 {.warning[UnusedImport]: off.}
