37a2b839f697fb9d310926585db97a5c5e56e8de
Dmitriy Ryajov <dryajov@gmail.com>
2026-02-17 15:31:31 -0600

    misc: format

--- kvstore/metrics.nim
+++ kvstore/metrics.nim
@@ -11,19 +11,27 @@ export metrics
 # tryPut (bulk, partial commit)
 declarePublicCounter(kvstore_tryput_total, "kvstore tryPut calls")
 declarePublicCounter(kvstore_tryput_retries_total, "kvstore tryPut retry iterations")
-declarePublicCounter(kvstore_tryput_exhausted_total, "kvstore tryPut max retries exhausted")
+declarePublicCounter(
+  kvstore_tryput_exhausted_total, "kvstore tryPut max retries exhausted"
+)
 
 # tryPutAtomic (all-or-nothing)
 declarePublicCounter(kvstore_tryputatomic_total, "kvstore tryPutAtomic calls")
-declarePublicCounter(kvstore_tryputatomic_retries_total, "kvstore tryPutAtomic retry iterations")
+declarePublicCounter(
+  kvstore_tryputatomic_retries_total, "kvstore tryPutAtomic retry iterations"
+)
 declarePublicCounter(
   kvstore_tryputatomic_exhausted_total, "kvstore tryPutAtomic max retries exhausted"
 )
 
 # tryDelete (bulk, partial commit)
 declarePublicCounter(kvstore_trydelete_total, "kvstore tryDelete calls")
-declarePublicCounter(kvstore_trydelete_retries_total, "kvstore tryDelete retry iterations")
-declarePublicCounter(kvstore_trydelete_exhausted_total, "kvstore tryDelete max retries exhausted")
+declarePublicCounter(
+  kvstore_trydelete_retries_total, "kvstore tryDelete retry iterations"
+)
+declarePublicCounter(
+  kvstore_trydelete_exhausted_total, "kvstore tryDelete max retries exhausted"
+)
 
 # tryDeleteAtomic (all-or-nothing)
 declarePublicCounter(kvstore_trydeleteatomic_total, "kvstore tryDeleteAtomic calls")
@@ -31,5 +39,6 @@ declarePublicCounter(
   kvstore_trydeleteatomic_retries_total, "kvstore tryDeleteAtomic retry iterations"
 )
 declarePublicCounter(
-  kvstore_trydeleteatomic_exhausted_total, "kvstore tryDeleteAtomic max retries exhausted"
+  kvstore_trydeleteatomic_exhausted_total,
+  "kvstore tryDeleteAtomic max retries exhausted",
 )
